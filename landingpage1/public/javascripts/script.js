17cd0b4a2afb00dcfee789db11afa5daac310504
BC029 <rainbow-life@outlook.fr>
2019-04-16 13:49:52 +0200

    Lnding Page 1

---
+++ landingpage1/public/javascripts/script.js
@@ -0,0 +1,50 @@
+var vue = new Vue({
+    el: "#widgetApp1",
+    data: () => {
+        return {
+            persons: "",
+            days: "",
+            carType: "",
+            email: "",
+            submitStatus: false,
+            submitMsg: "",
+            saveInputs: {
+                persons: [],
+                numPersons: [],
+                days: [],
+                carType: [],
+                email: []
+            },
+            submits: {
+                customer: []
+            }
+        }
+    },
+    methods: {
+        saveSubmit: function() {
+            this.submits.customer.push({
+                Option: this.persons,
+                Forfait: this.days,
+                Voiture: this.carType,
+                Email: this.email
+            });
+            this.submitStatus = true;
+            this.submitMsg = "Nous reviendrons vers vous avec votre devis rapidement.";
+            console.log(this.submits);
+        }
+    },
+    watch: {
+        persons(value) {
+            console.log(value);
+            this.saveInputs.persons.push(value);
+        },
+        days(value) {
+            console.log(value);
+            this.saveInputs.days.push(value);
+        },
+        carType(value) {
+            console.log(value);
+            this.saveInputs.carType.push(value);
+        }
+    }
+})
